e88fda2ee180ce24c9130b1d2100eb3384e35eac
Felipe <felipe.stefani.correia@gmail.com>
2020-09-23 21:14:22 -0300

    spawn script

---
+++ Assets/ProjectTeamFramework/Scripts/Utilities/SpawnEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public float xPos;
+    public float spawnRadius = 7;
+    public float zPos;
+    public int enemyCount;
+    public float spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+    IEnumerator EnemyDrop()
+    {
+       while (enemyCount < 10)
+        {
+            //TODO FAZER A PORRA DO ENEMY APARECER NA FRENTE
+            Vector3 spawnPos = GameObject.Find("Player").transform.forward*20;
+            xPos = Random.Range(-10, 10);           
+            Instantiate(theEnemy, new Vector3(xPos,1, spawnPos.z), Quaternion.identity);
+            yield return new WaitForSeconds(0.9f);
+            enemyCount += 1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
